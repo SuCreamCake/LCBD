15f9366ebbf0476903bf03156ebc186241a73d50
leejy1685 <ucc1685@naver.com>
2023-06-19 21:48:21 +0900

    23.06.19
    공격속도 구현, 코드 정리

--- LCBD/Assets/Scripts/Player.cs
+++ LCBD/Assets/Scripts/Player.cs
@@ -29,7 +29,10 @@ public class Player : MonoBehaviour
     public int luck;
 
     //음파 오브젝트
+    private float time = 0;
     public GameObject soundWave;
+    //스테이지
+    public int stage;
 
     
 
@@ -39,60 +42,31 @@ public class Player : MonoBehaviour
         //camera = GameObject.Find("Main Camera").GetComponent<Camera>();  
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        infancy();
+        if(stage == 1)
+            infancy();
     }
 
     private void Update()
     {
-        //Jump
-        if (Input.GetButtonDown("Jump"))
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        //stop speed
-        if (Input.GetButtonUp("Horizontal"))
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-        //Direction Sprite
-        if (Input.GetButtonDown("Horizontal"))
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-
-
-        //attack
-        Vector3 point = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
-        Input.mousePosition.y, -camera.transform.position.z));
-        if (Input.GetMouseButtonDown(0))
-        { 
-      
-            soundWave.transform.position = new Vector2(point.x, point.y);
-            if (crossroads < Mathf.Sqrt(Mathf.Pow(point.x - this.transform.position.x, 2) + Mathf.Pow(point.y - this.transform.position.y, 2)))
-            {
-                
-                float maxCrossroads = crossroads / Mathf.Sqrt(Mathf.Pow(point.x - this.transform.position.x, 2) + Mathf.Pow(point.y - this.transform.position.y, 2));
-
-                soundWave.transform.position = new Vector2(this.transform.position.x+(point.x - this.transform.position.x) * maxCrossroads  
-                    , this.transform.position.y + (point.y - this.transform.position.y) * maxCrossroads );
-            }
-            
-            Instantiate(soundWave);
+        jump();
+        stopSpeed();
+        directionSprite();
+        switch (stage)
+        {
+            case 1:
+                attack();
+                break;
+            default:
+                break;
         }
     }
 
 
     private void FixedUpdate()
     {
-        //Move Speed
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        work();
 
-        //maxSpeed
-        if (rigid.velocity.x > maxSpeed)
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < (-1) * maxSpeed)
-            rigid.velocity = new Vector2((-1) * maxSpeed, rigid.velocity.y);
-
-        if (isLadder)
-        {
-            float ver = Input.GetAxis("Vertical");
-            rigid.velocity = new Vector2(rigid.velocity.x, ver * maxSpeed);
-        }
+        upDown();
 
     }
 
@@ -111,6 +85,69 @@ public class Player : MonoBehaviour
         {
             isLadder = false;
             rigid.gravityScale = 2;
+        }
+    }
+
+    private void jump()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+    private void stopSpeed()
+    {
+        //stop speed
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+    }
+    private void directionSprite()
+    {
+        //Direction Sprite
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+    }
+    private void attack()
+    {
+        //attack
+        time += Time.deltaTime;
+        Debug.Log(time);
+        Vector3 point = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+        Input.mousePosition.y, -camera.transform.position.z));
+        if (time >= attackSpeed && Input.GetMouseButtonDown(0))
+        {
+            time = 0;
+            soundWave.transform.position = new Vector2(point.x, point.y);
+            if (crossroads < Mathf.Sqrt(Mathf.Pow(point.x - this.transform.position.x, 2) + Mathf.Pow(point.y - this.transform.position.y, 2)))
+            {
+
+                float maxCrossroads = crossroads / Mathf.Sqrt(Mathf.Pow(point.x - this.transform.position.x, 2) + Mathf.Pow(point.y - this.transform.position.y, 2));
+
+                soundWave.transform.position = new Vector2(this.transform.position.x + (point.x - this.transform.position.x) * maxCrossroads
+                    , this.transform.position.y + (point.y - this.transform.position.y) * maxCrossroads);
+            }
+
+            Instantiate(soundWave);
+        }
+    }
+    private void work()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //maxSpeed
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < (-1) * maxSpeed)
+            rigid.velocity = new Vector2((-1) * maxSpeed, rigid.velocity.y);
+    }
+    private void upDown()
+    {
+        //Updown
+        if (isLadder)
+        {
+            float ver = Input.GetAxis("Vertical");
+            rigid.velocity = new Vector2(rigid.velocity.x, ver * maxSpeed);
         }
     }
 
@@ -136,5 +173,11 @@ public class Player : MonoBehaviour
         crossroads = 3;
         //행운
         luck = 50 + 20;
+
+    }
+
+    private void childhood()
+    {
+
     }
 }
