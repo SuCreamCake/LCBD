dc708b79fbc53c3f44f774ca4f4c4911d1886a56
Dongju20 <114079607+Dongju20@users.noreply.github.com>
2023-07-22 18:19:23 +0900

    배틀로직작성
    f1f2f3누를 시 공격 타입 바꾸고 공격함
    battlelogic을 이용해 공격 타입 바꿔줌
    방어 방법은 미구현

--- LCBD/Assets/Scripts/Bullet.cs
+++ LCBD/Assets/Scripts/Bullet.cs
@@ -6,12 +6,23 @@ public class Bullet : MonoBehaviour
 {
     //원거리 공격 데미지
     public int damage;
+    private Rigidbody2D bulletRigidbody2D;
+    private float bulletSpeed = 10f;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Obstacle") //여기 투사체 또는 적대 세력이 들어감)
+        if (collision.gameObject.tag == "Enemy") //여기 투사체 또는 적대 세력이 들어감)
             Destroy(gameObject);
-        else if (collision.gameObject.tag == "Enemy")
+        else if (collision.gameObject.tag == "Wall")
             Destroy(gameObject);
-
+    }
+    private void Start()
+    {
+        bulletRigidbody2D = GetComponent<Rigidbody2D>();
+        bulletRigidbody2D.velocity = bulletSpeed * transform.right;
+        //자기 자신을 삭제한 메서드
+        if (gameObject != null)
+        {
+            Destroy(gameObject, 10);
+        }
     }
 }
