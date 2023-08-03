60c4ac2a279b4c4e447d9a9b6eb723e81c9aa969
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2023-08-04 03:34:28 +0900

    0802-0803
    스테이지에서 필드 배치하는 건 기획서대로는 구현이 어려워서 인터넷에 서칭하면서 찾은 방식대로 시작지점에서 퍼져나가면서 필요한 필드의 개수만큼 만들 수 있게 했음.
    
    StageGenerator.cs 수정
    현재 스테이지 레벨(1~5) 관련
    -> _stageLevel 변수 추가
    -> StageLevel 프로퍼티 추가
    생성할 필드의 개수 관련
    -> FieldCount 프로퍼티 추가
    
    FieldCount는 StageLevel을 통해 구하고,
    FieldSquareMatrixRow는 FieldCount를 통해 구하도록 변경
    
    GenerateFields() 메소드에서
    랜덤으로 시작되는 필드를 잡고
    주변으로 이동하면서 만들어야하는 필드의 수만큼 생성할 때까지 빈 필드이면 일반 필드로 변경함.
    
    근데 이 부분이 무한 루프를 돌 수도 있을 것 같아서, 혹시 몰라서 loopCount 를 돌려서 1000 이상이면 그냥 break시킴.
    
    그 다음 보스 필드를 선택해야 하는데, 시작 필드와 가장 먼 필드를 정하려고 함.
    (아직 구현 중)
    
    그 다음 일정 확률로 일반필드를 특수필드로 변경함
    
    GetFieldsPoints() 메소드에서 각 필드를 담아서 리스트로 리턴함
    ConnectFields() 메소드에서 각 필드를 연결하는 정보를 담아서 리스트로 리턴함
    GetFieldsPointsEdges() 메소드는 ConnectFields()와 같고, 각 필드의 연결을 엣지처럼 생각
    CalcPathCost() 메소드에서 시작필드로부터 각 필드까지의 거리를 계산한다. (구현 중)
    FindLongestPath() 메소드는 CalcPathCost()를 통해 구한 최소 경로중 가장 비용이 많이 드는 필드를 리턴
    
    현재 길찾기 알고리즘 구현이 안되어서, 보스필드가 ConnectFields()에 제일 처음 저장된 필드로 지정되는 것 같음.
    
    FieldPoint.cs 생성
    그리고, 각 필드의 좌표를 저장할 수 있는 클래스를 만듦.

---
+++ LCBD/Assets/Wankyu_Map/wk_Scripts/FieldPoint.cs
@@ -0,0 +1,18 @@
+﻿//필드의 X Y 좌표를 담는 클래스
+class FieldPoint
+{
+    private int x;
+    private int y;
+
+    public FieldPoint(int x = 0, int y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int X { get { return x; } }
+    public int Y { get { return y; } }
+
+    public void SetX(int x) { this.x = x; }
+    public void SetY(int y) { this.y = y; }
+}
--- LCBD/Assets/Wankyu_Map/wk_Scripts/StageGenerator.cs
+++ LCBD/Assets/Wankyu_Map/wk_Scripts/StageGenerator.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class StageGenerator : MonoBehaviour
 {
-    [SerializeField, Range(6, 10)]
+    [SerializeField, Range(1, 5)]
+    private int _stageLevel;
+    public int StageLevel { get { return _stageLevel; } private set { _stageLevel = value; } }
+
+    public int FieldCount { get; private set; }
+
     private int _fieldSquareMatrixRow;
     public int FieldSquareMatrixRow
     {
         get { return _fieldSquareMatrixRow; }
-        private set { if (6 <= value && value <= 10) _fieldSquareMatrixRow = value; }
+        private set { if (6 <= value && value <= 8) _fieldSquareMatrixRow = value; }
     }
 
     [SerializeField, Range(20, 100)]
@@ -22,12 +25,12 @@ public class StageGenerator : MonoBehaviour
 
     public void SetMapWidth(int mapWidth)
     {
-        if(mapWidth > 20)
+        if (mapWidth > 20)
             MapWidth = mapWidth;
     }
     public void SetMapHeight(int mapHeight)
     {
-        if(mapHeight > 20)
+        if (mapHeight > 20)
             MapHeight = mapHeight;
     }
 
@@ -35,11 +38,15 @@ public class StageGenerator : MonoBehaviour
 
     MapGenerator[,] mapGenerator;
     public MapGenerator[,] GetMapGenerator() { return mapGenerator; }
-    
-
 
     private void Start()
     {
+        FieldCount = 5 * StageLevel + UnityEngine.Random.Range(3, 6);
+        FieldSquareMatrixRow = Mathf.FloorToInt(Mathf.Sqrt(FieldCount + 20)) + 1;
+
+        Debug.Log("roomCount, stageLevel : " + FieldCount + ", " + StageLevel);
+        Debug.Log("FieldSquareMatrixRow : " + FieldSquareMatrixRow);
+
         GenerateFields();
         DrawFields();
     }
@@ -49,6 +56,12 @@ public class StageGenerator : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Minus)) // '-' 키
         {
+            FieldCount = 5 * StageLevel + UnityEngine.Random.Range(3, 6);
+            FieldSquareMatrixRow = Mathf.FloorToInt(Mathf.Sqrt(FieldCount + 20)) + 1;
+
+            Debug.Log("roomCount, stageLevel : " + FieldCount + ", " + StageLevel);
+            Debug.Log("FieldSquareMatrixRow : " + FieldSquareMatrixRow);
+
             GenerateFields();
             DrawFields();
         }
@@ -56,6 +69,7 @@ public class StageGenerator : MonoBehaviour
 
     public void DrawFields()
     {
+        Debug.Log("DrawFields()");
         mapGenerator = new MapGenerator[FieldSquareMatrixRow, FieldSquareMatrixRow];
 
         for (int x = 0; x < FieldSquareMatrixRow; x++)
@@ -63,7 +77,7 @@ public class StageGenerator : MonoBehaviour
             for (int y = 0; y < FieldSquareMatrixRow; y++)
             {
                 //임시 
-                fieldType[x,y] = (FieldType)Enum.GetValues(typeof(FieldType)).GetValue(new System.Random().Next(0, Enum.GetValues(typeof(FieldType)).Length));
+                //fieldType[x,y] = (FieldType)Enum.GetValues(typeof(FieldType)).GetValue(new System.Random().Next(0, Enum.GetValues(typeof(FieldType)).Length));
 
                 mapGenerator[x, y] = new MapGenerator();
                 mapGenerator[x, y].InitMap(MapWidth, MapHeight, fieldType[x, y]);
@@ -71,15 +85,219 @@ public class StageGenerator : MonoBehaviour
         }
     }
 
-    public void GenerateFields()
+    private void InitFields()
     {
         fieldType = new FieldType[FieldSquareMatrixRow, FieldSquareMatrixRow];
 
-
-
-
-        
+        for (int i = 0; i < fieldType.GetLength(0); i++)
+        {
+            for (int j = 0; j < fieldType.GetLength(1); j++)
+            {
+                fieldType[i, j] = FieldType.None;
+            }
+        }
     }
+
+    public void GenerateFields()
+    {
+        InitFields();
+        Debug.Log("GenerateFields()");
+
+        int startRow = UnityEngine.Random.Range(0, fieldType.GetLength(0));
+        int startCol = UnityEngine.Random.Range(0, fieldType.GetLength(1));
+
+        Debug.Log("startRow, startCol : " + startRow + ", " + startCol);
+
+        int row = startRow;
+        int col = startCol;
+
+        //시작 필드
+        fieldType[startRow, startCol] = FieldType.Start;
+
+        int count = 1;  //생성된 필드의 수
+
+        //일반 필드 생성
+        int loopCount = 0;  //while문 loop Count
+        while (count < FieldCount)
+        {
+            int direction = UnityEngine.Random.Range(1, 5); // 1. ↑ / 2. ↓ / 3. → / 4. ←
+            switch (direction)
+            {
+                case 1:
+                    if (row < FieldSquareMatrixRow - 1)
+                        row++;
+                    break;
+                case 2:
+                    if (row > 0)
+                        row--;
+                    break;
+                case 3:
+                    if (col < FieldSquareMatrixRow - 1)
+                        col++;
+                    break;
+                case 4:
+                    if (col > 0)
+                        col--;
+                    break;
+            }
+            if (fieldType[row, col] == FieldType.None)
+            {
+                Debug.Log("generated/ row, col : " + row + ", " + col);
+                fieldType[row, col] = FieldType.Common;
+                count++;
+            }
+
+            loopCount++;
+
+            Debug.Log("row, col : " + row + ", " + col);
+
+            if (loopCount >= 1000)
+                break;
+        }
+
+        //하나의 필드를 보스필드로 변경
+        //시작 필드로부터 가장 먼 필드를 보스 필드로 변경
+        FieldPoint fartheatField = FindLongestPath();
+        fieldType[fartheatField.X, fartheatField.Y] = FieldType.Boss;
+
+        //일정 확률로 일반필드를 특수필드로 변경
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] == FieldType.Common)
+                {
+                    if (UnityEngine.Random.Range(0, 5) == 0) // 0~4 중 0 이므로 1/5 => 20%
+                        fieldType[x, y] = FieldType.Special;
+                }
+            }
+        }
+    }
+
+
+    //None 필드를 제외한 필드의 좌표를 리스트로 리턴하는 메소드
+    private List<FieldPoint> GetFieldsPoints()
+    {
+        List<FieldPoint> fieldsVertexs = new List<FieldPoint>();
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] != FieldType.None)
+                {
+                    fieldsVertexs.Add(new FieldPoint(x, y));
+                }
+            }
+        }
+
+        return fieldsVertexs;
+    }
+
+    //None 필드를 제외한 각 필드 간의 연결되는 두 필드를 쌍으로 리스트에 담고 리턴하는 메소드
+    private List<Tuple<FieldPoint, FieldPoint>> ConnectFields()
+    {
+        return GetFieldsPointsEdges();
+    }
+
+    private List<Tuple<FieldPoint, FieldPoint>> GetFieldsPointsEdges()
+    {
+        List<Tuple<FieldPoint, FieldPoint>> linkedFields = new List<Tuple<FieldPoint, FieldPoint>>();
+
+        //가로 연결
+        for (int x = 0; x < FieldSquareMatrixRow - 1; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] != FieldType.None && fieldType[x + 1, y] != FieldType.None)
+                {
+                    FieldPoint field1 = new(x, y);
+                    FieldPoint field2 = new(x + 1, y);
+
+                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
+                    linkedFields.Add(linkedField);
+                }
+            }
+        }
+
+        //세로 연결
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow - 1; y++)
+            {
+                if (fieldType[x, y] != FieldType.None && fieldType[x, y + 1] != FieldType.None)
+                {
+                    FieldPoint field1 = new(x, y);
+                    FieldPoint field2 = new(x, y + 1);
+
+                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
+                    linkedFields.Add(linkedField);
+                }
+            }
+        }
+
+        return linkedFields;
+    }
+
+    //시작 필드 부터 다른 특정 필드 간의 거리를 계산하는 메소드
+    // TODO 길찰기 알고리즘 필요
+    private int CalcPathCost(FieldPoint otherPoint)
+    {
+        int cost = 0;
+        FieldPoint startPoint = new FieldPoint();
+
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] == FieldType.Start)
+                {
+                    startPoint.SetX(x);
+                    startPoint.SetY(y);
+                }
+            }
+        }
+
+        List<FieldPoint> fieldsVertexs = new List<FieldPoint>();
+        foreach (var item in GetFieldsPoints())
+        {
+            fieldsVertexs.Add(item);
+        }
+
+        List<Tuple<FieldPoint, FieldPoint>> fieldsEdges = new List<Tuple<FieldPoint, FieldPoint>>();
+        foreach (var item in GetFieldsPointsEdges())
+        {
+            fieldsEdges.Add(item);
+        }
+
+        return cost;
+    }
+
+
+
+    //시작 필드로부터 제일 먼 필드를 구하는 메소드
+    private FieldPoint FindLongestPath()
+    {
+        int highestCost = 0;
+        int highestIndex = 0;
+
+        int index = 0;
+        foreach (var node in GetFieldsPoints())
+        {
+            int cost = CalcPathCost(node);
+            if (cost > highestCost)
+            {
+                highestCost = cost;
+                highestIndex = index;
+            }
+
+            index++;
+        }
+
+        return GetFieldsPoints().ElementAt(highestIndex);
+    }
+
+
+
 
     //디버그
     void OnDrawGizmos()
@@ -90,7 +308,7 @@ public class StageGenerator : MonoBehaviour
             {
                 for (int j = 0; j < mapGenerator.GetLength(1); j++)
                 {
-                    if(mapGenerator[i, j] !=  null)
+                    if (mapGenerator[i, j] != null)
                     {
                         for (int x = 0; x < mapGenerator[i, j].Fields.Map.GetLength(0); x++)
                         {
