6237f701aeef9b35f547e1495cbba54bd9d25ba6
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2023-08-05 22:10:54 +0900

    0805
    CalcPathCost() 에서 길찾기 알고리즘 적용함으로써 필드 까지의 비용을 구할 수 있게 했고,
    FindLongestPath()에서 가장 비싼(가장 먼) 필드 필드 리턴 하게 해서, 시작 필드로부터 가장 먼 필드를 보스 필드로 지정.
    
    FieldPoint.cs 수정 / 길찾기 알고리즘에 이용할 리스트와 큐나 딕셔너리에 Contains()가 안되거나 Key가 중복되는데 감지 못하는 현상을 고치기 위해서 Equals()와 GetHashCode() override하여 재정의했음.
     Equals() override 추가
     GetHashCode() override 추가
     ToString() override 추가 / 이건 디버그할 때 편하려고 override함.
    
    StageGenerator.cs 수정
    각 변수 및 메소드에 주석 추가
    
    StartRow, StartCol 프로퍼티 추가 - 시작 필드의 열과 행
    
    fieldsPointsEdges 리스트 추가 - None 필드를 제외한 각 필드 간의 연결되는 두 필드를 쌍으로 갖는 리스트
    InitFieldsPointsEdges() 메소드 추가 - 리스트 초기화 및 연결된 필드들을 저장하는 메소드
    
    fieldsPointsVertex 리스트 추가 - None 필드를 제외한 필드들을 저장하는 리스트
    InitFieldsPointsVertex() 메소드 추가 - 리스트 초기화 및 None 필드를 제외한 필드들을 저장하는 메소드
    
    CalcPathCost() 수정 - 메소드 구현 완료 - 시작필드로부터 각 필드까지의 거리를 계산한다. (길찾기 알고리즘(아마 BFS알고리즘, 내가 본 건 Floodfill 알고리즘인데, 내용은 BFS였던거 같음))
    -> 스테이지 생성 시 시작 필드로부터 가장 먼 필드를 보스 필드로 지정할 수 있게 함.
    ConnectFields() 삭제 - InitFieldsPointsEdges() 같은 역할이었어서 삭제함.

--- LCBD/Assets/Wankyu_Map/wk_Scripts/FieldPoint.cs
+++ LCBD/Assets/Wankyu_Map/wk_Scripts/FieldPoint.cs
@@ -1,4 +1,6 @@
 ﻿//필드의 X Y 좌표를 담는 클래스
+using System;
+
 class FieldPoint
 {
     private int x;
@@ -15,4 +17,27 @@ class FieldPoint
 
     public void SetX(int x) { this.x = x; }
     public void SetY(int y) { this.y = y; }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as FieldPoint);
+    }
+
+    public bool Equals(FieldPoint otherField)
+    {
+        return otherField != null &&
+            x == otherField.x &&
+            y == otherField.y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
+
+    public override string ToString()
+    {
+        string msg = "(" + x + ", " + y + ")";
+        return msg;
+    }
 }
--- LCBD/Assets/Wankyu_Map/wk_Scripts/StageGenerator.cs
+++ LCBD/Assets/Wankyu_Map/wk_Scripts/StageGenerator.cs
@@ -6,20 +6,15 @@ using UnityEngine;
 public class StageGenerator : MonoBehaviour
 {
     [SerializeField, Range(1, 5)]
-    private int _stageLevel;
+    private int _stageLevel;    //스테이지 레벨
     public int StageLevel { get { return _stageLevel; } private set { _stageLevel = value; } }
 
-    public int FieldCount { get; private set; }
+    public int FieldCount { get; private set; } //필드의 개수
 
-    private int _fieldSquareMatrixRow;
-    public int FieldSquareMatrixRow
-    {
-        get { return _fieldSquareMatrixRow; }
-        private set { if (6 <= value && value <= 8) _fieldSquareMatrixRow = value; }
-    }
+    public int FieldSquareMatrixRow { get; private set; } //필드의 NxN 배열의 N(행 수)
 
     [SerializeField, Range(20, 100)]
-    private int _mapWidth, _mapHeight;
+    private int _mapWidth, _mapHeight;  //한 맵(필드)의 너비, 높이
     public int MapWidth { get { return _mapWidth; } private set { _mapWidth = value; } }
     public int MapHeight { get { return _mapHeight; } private set { _mapHeight = value; } }
 
@@ -34,9 +29,72 @@ public class StageGenerator : MonoBehaviour
             MapHeight = mapHeight;
     }
 
-    FieldType[,] fieldType;
+    public int StartRow { get; private set; }   //시작필드의 행
+    public int StartCol { get; private set; }   //시작필드의 열
 
-    MapGenerator[,] mapGenerator;
+    private List<Tuple<FieldPoint, FieldPoint>> fieldsPointsEdges;  //None 필드를 제외한 각 필드 간의 연결되는 두 필드(주변 필드)들을 쌍으로 저장하는 리스트
+
+    //fieldsPointsEdges 를 초기화하고 연결되는 필드(주변 필드)들을 모두 저장하는 메소드
+    private void InitFieldsPointsEdges()
+    {
+        fieldsPointsEdges = new List<Tuple<FieldPoint, FieldPoint>>();
+
+        //가로 연결
+        for (int x = 0; x < FieldSquareMatrixRow - 1; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] != FieldType.None && fieldType[x + 1, y] != FieldType.None)
+                {
+                    FieldPoint field1 = new(x, y);
+                    FieldPoint field2 = new(x + 1, y);
+
+                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
+                    fieldsPointsEdges.Add(linkedField);
+                }
+            }
+        }
+
+        //세로 연결
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow - 1; y++)
+            {
+                if (fieldType[x, y] != FieldType.None && fieldType[x, y + 1] != FieldType.None)
+                {
+                    FieldPoint field1 = new(x, y);
+                    FieldPoint field2 = new(x, y + 1);
+
+                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
+                    fieldsPointsEdges.Add(linkedField);
+                }
+            }
+        }
+    }
+
+    private List<FieldPoint> fieldsPointsVertex;    //None 필드를 제외한 필드들을 저장하는 리스트
+
+    //fieldsPointsVertex를 초기화하고 None 필드를 제외한 필드의 좌표를 저장하는 메소드
+    private void InitFieldsPointsVertex()
+    {
+        fieldsPointsVertex = new List<FieldPoint>();
+        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        {
+            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            {
+                if (fieldType[x, y] != FieldType.None)
+                {
+                    fieldsPointsVertex.Add(new FieldPoint(x, y));
+                }
+            }
+        }
+    }
+
+
+    FieldType[,] fieldType; //각 맵의 필드 타입
+
+    MapGenerator[,] mapGenerator;   //각 맵(필드)의 생성기
+
     public MapGenerator[,] GetMapGenerator() { return mapGenerator; }
 
     private void Start()
@@ -67,9 +125,9 @@ public class StageGenerator : MonoBehaviour
         }
     }
 
+    //각 맵의 필드 타입에 맞게 맵 생성하는 메소드
     public void DrawFields()
     {
-        Debug.Log("DrawFields()");
         mapGenerator = new MapGenerator[FieldSquareMatrixRow, FieldSquareMatrixRow];
 
         for (int x = 0; x < FieldSquareMatrixRow; x++)
@@ -85,6 +143,7 @@ public class StageGenerator : MonoBehaviour
         }
     }
 
+    //모든 맵을 초기화 하는 메소드
     private void InitFields()
     {
         fieldType = new FieldType[FieldSquareMatrixRow, FieldSquareMatrixRow];
@@ -98,25 +157,23 @@ public class StageGenerator : MonoBehaviour
         }
     }
 
+    //스테이지의 맵을 배치하고 각 맵의 필드 타입을 지정하는 메소드
     public void GenerateFields()
     {
         InitFields();
-        Debug.Log("GenerateFields()");
 
-        int startRow = UnityEngine.Random.Range(0, fieldType.GetLength(0));
-        int startCol = UnityEngine.Random.Range(0, fieldType.GetLength(1));
+        StartRow = UnityEngine.Random.Range(0, fieldType.GetLength(0));
+        StartCol = UnityEngine.Random.Range(0, fieldType.GetLength(1));
 
-        Debug.Log("startRow, startCol : " + startRow + ", " + startCol);
+        int curRow = StartRow;
+        int curCol = StartCol;
 
-        int row = startRow;
-        int col = startCol;
-
-        //시작 필드
-        fieldType[startRow, startCol] = FieldType.Start;
+        //시작 필드 생성 부분
+        fieldType[StartRow, StartCol] = FieldType.Start;
 
         int count = 1;  //생성된 필드의 수
 
-        //일반 필드 생성
+        //일반 필드 생성 부분
         int loopCount = 0;  //while문 loop Count
         while (count < FieldCount)
         {
@@ -124,43 +181,46 @@ public class StageGenerator : MonoBehaviour
             switch (direction)
             {
                 case 1:
-                    if (row < FieldSquareMatrixRow - 1)
-                        row++;
+                    if (curRow < FieldSquareMatrixRow - 1)
+                        curRow++;
                     break;
                 case 2:
-                    if (row > 0)
-                        row--;
+                    if (curRow > 0)
+                        curRow--;
                     break;
                 case 3:
-                    if (col < FieldSquareMatrixRow - 1)
-                        col++;
+                    if (curCol < FieldSquareMatrixRow - 1)
+                        curCol++;
                     break;
                 case 4:
-                    if (col > 0)
-                        col--;
+                    if (curCol > 0)
+                        curCol--;
                     break;
             }
-            if (fieldType[row, col] == FieldType.None)
+            if (fieldType[curRow, curCol] == FieldType.None)
             {
-                Debug.Log("generated/ row, col : " + row + ", " + col);
-                fieldType[row, col] = FieldType.Common;
+                fieldType[curRow, curCol] = FieldType.Common;
                 count++;
             }
 
             loopCount++;
 
-            Debug.Log("row, col : " + row + ", " + col);
-
             if (loopCount >= 1000)
                 break;
         }
 
-        //하나의 필드를 보스필드로 변경
-        //시작 필드로부터 가장 먼 필드를 보스 필드로 변경
+        //모든 필드를 생성했으므로, 보스 필드 지정 전에
+        //None 이 아닌 모든 필드들의 리스트와
+        //None 이 아닌 모든 필드들의 연결을 초기화하여 저장해야 함.
+        InitFieldsPointsVertex();
+        InitFieldsPointsEdges();
+
+        //하나의 필드를 보스필드로 변경 부분
+        //시작 필드로부터 가장 먼 필드를 보스 필드로 변경하도록 함.
         FieldPoint fartheatField = FindLongestPath();
         fieldType[fartheatField.X, fartheatField.Y] = FieldType.Boss;
 
-        //일정 확률로 일반필드를 특수필드로 변경
+        //일정 확률로 일반필드를 특수필드로 변경 부분
         for (int x = 0; x < FieldSquareMatrixRow; x++)
         {
             for (int y = 0; y < FieldSquareMatrixRow; y++)
@@ -172,119 +232,95 @@ public class StageGenerator : MonoBehaviour
                 }
             }
         }
+
     }
 
 
-    //None 필드를 제외한 필드의 좌표를 리스트로 리턴하는 메소드
-    private List<FieldPoint> GetFieldsPoints()
-    {
-        List<FieldPoint> fieldsVertexs = new List<FieldPoint>();
-        for (int x = 0; x < FieldSquareMatrixRow; x++)
-        {
-            for (int y = 0; y < FieldSquareMatrixRow; y++)
-            {
-                if (fieldType[x, y] != FieldType.None)
-                {
-                    fieldsVertexs.Add(new FieldPoint(x, y));
-                }
-            }
-        }
-
-        return fieldsVertexs;
-    }
-
-    //None 필드를 제외한 각 필드 간의 연결되는 두 필드를 쌍으로 리스트에 담고 리턴하는 메소드
-    private List<Tuple<FieldPoint, FieldPoint>> ConnectFields()
-    {
-        return GetFieldsPointsEdges();
-    }
-
-    private List<Tuple<FieldPoint, FieldPoint>> GetFieldsPointsEdges()
-    {
-        List<Tuple<FieldPoint, FieldPoint>> linkedFields = new List<Tuple<FieldPoint, FieldPoint>>();
-
-        //가로 연결
-        for (int x = 0; x < FieldSquareMatrixRow - 1; x++)
-        {
-            for (int y = 0; y < FieldSquareMatrixRow; y++)
-            {
-                if (fieldType[x, y] != FieldType.None && fieldType[x + 1, y] != FieldType.None)
-                {
-                    FieldPoint field1 = new(x, y);
-                    FieldPoint field2 = new(x + 1, y);
-
-                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
-                    linkedFields.Add(linkedField);
-                }
-            }
-        }
-
-        //세로 연결
-        for (int x = 0; x < FieldSquareMatrixRow; x++)
-        {
-            for (int y = 0; y < FieldSquareMatrixRow - 1; y++)
-            {
-                if (fieldType[x, y] != FieldType.None && fieldType[x, y + 1] != FieldType.None)
-                {
-                    FieldPoint field1 = new(x, y);
-                    FieldPoint field2 = new(x, y + 1);
-
-                    Tuple<FieldPoint, FieldPoint> linkedField = new(field1, field2);
-                    linkedFields.Add(linkedField);
-                }
-            }
-        }
-
-        return linkedFields;
-    }
 
     //시작 필드 부터 다른 특정 필드 간의 거리를 계산하는 메소드
-    // TODO 길찰기 알고리즘 필요
+    // 길 찾기 알고리즘 이용
     private int CalcPathCost(FieldPoint otherPoint)
     {
-        int cost = 0;
-        FieldPoint startPoint = new FieldPoint();
+        FieldPoint startPoint = new(StartRow, StartCol);    //시작 필드
 
-        for (int x = 0; x < FieldSquareMatrixRow; x++)
+        if (otherPoint.Equals(startPoint))
+        {   //("otherField가 startField가 같다면 비용은 0")
+            return 0;
+        }
+
+        Queue<FieldPoint> frontier = new Queue<FieldPoint>();   //방문할 필드 큐
+        List<FieldPoint> visited = new List<FieldPoint>();      //방문한 필드 리스트
+        Dictionary<FieldPoint, FieldPoint> nextFields = new Dictionary<FieldPoint, FieldPoint>();    //다음 방문할 필드 Dict [K:지금 필드, V:다음 필드];
+
+        frontier.Enqueue(otherPoint);   //처음에 도착할 필드를 큐에 넣음
+
+        while (frontier.Count > 0)
         {
-            for (int y = 0; y < FieldSquareMatrixRow; y++)
+            FieldPoint curField = frontier.Dequeue();   //제일 앞에 큐에서 꺼낸 필드로 현재 필드
+
+            foreach (FieldPoint neighbor in GetListNeighborFieldPoints(curField))   //현재 필드의 주변(이웃) 필드 만큼
             {
-                if (fieldType[x, y] == FieldType.Start)
+                //처음에 Contains() 가 제대로 작동도 안하고, 확인해보니까 dictionary에 키도 중복으로 추가되어서, 중복 방지 및 isError로 처리하려니까 너무 복잡해지고 이상해서 찾아보니까
+                // FieldPoint class 의 GetHashCode()를 overriding 해 주어야 했다. 이제 잘 됨.
+                if (!visited.Contains(neighbor) && !frontier.Contains(neighbor))    //주변 필드가 방문한 필드 리스트에 없고, 방문할 필드 큐에도 들어 있지 않을 때,
                 {
-                    startPoint.SetX(x);
-                    startPoint.SetY(y);
+                    frontier.Enqueue(neighbor);         //이웃 필드를 큐에 넣고
+                    nextFields.Add(neighbor, curField); //사전에도 적어줌. [이웃, 현재] => 이헐게 해야 시작 필드부터 도착 필드까지 차례대로 갈 수 있음.
                 }
+
             }
+            visited.Add(curField);  //모든 이웃 필드를 처리한 후 방문한 필드에 현재 필드 넣음.
         }
 
-        List<FieldPoint> fieldsVertexs = new List<FieldPoint>();
-        foreach (var item in GetFieldsPoints())
+        if (visited.Contains(startPoint) == false)
         {
-            fieldsVertexs.Add(item);
+            Debug.Log("startPoint에 도달 불가"); //이 코드에서는 필드가 떨어질 수 없게 짜 놓았기 때문에 뜨면 안되지만, 혹시 몰라서 작성함.
+            return -1;
         }
 
-        List<Tuple<FieldPoint, FieldPoint>> fieldsEdges = new List<Tuple<FieldPoint, FieldPoint>>();
-        foreach (var item in GetFieldsPointsEdges())
+        //거리 최소 경로 비용 계산
+        int cost = 0;   //비용
+        FieldPoint curPathField = startPoint;   //현재 필드
+        while (!(curPathField.X == otherPoint.X && curPathField.Y == otherPoint.Y)) //nextFieldDict를 통해 시작 필드부터 지정 경로까지 도착할 때까지 돌림
         {
-            fieldsEdges.Add(item);
+            curPathField = nextFields[curPathField];    //사전 통해서 경로 따라 가면서
+            cost++; // 거리 비용은 모두 1이므로 읽어가면서 ++ 해줌
         }
 
         return cost;
     }
 
+    //주어진 필드의 이웃한 필드들을 리턴하는 메소드
+    List<FieldPoint> GetListNeighborFieldPoints(FieldPoint fieldPoint)
+    {
+        List<FieldPoint> neighborList = new List<FieldPoint>();
 
+        foreach (var tuple in fieldsPointsEdges)
+        {
+            if (tuple.Item1.X ==  fieldPoint.X && tuple.Item1.Y == fieldPoint.Y)
+            {
+                neighborList.Add(tuple.Item2);
+            }
+            else if (tuple.Item2.X == fieldPoint.X && tuple.Item2.Y == fieldPoint.Y)
+            {
+                neighborList.Add(tuple.Item1);
+            }
+        }
 
-    //시작 필드로부터 제일 먼 필드를 구하는 메소드
+        return neighborList;
+    }
+
+    //시작 필드로부터 제일 먼 필드를 구하는 메소드 (같으면 제일 처음)
     private FieldPoint FindLongestPath()
     {
         int highestCost = 0;
         int highestIndex = 0;
 
         int index = 0;
-        foreach (var node in GetFieldsPoints())
+        foreach (var node in fieldsPointsVertex)
         {
             int cost = CalcPathCost(node);
-            if (cost > highestCost)
+            if (cost > 0 && cost > highestCost)
             {
                 highestCost = cost;
                 highestIndex = index;
@@ -293,7 +329,8 @@ public class StageGenerator : MonoBehaviour
             index++;
         }
 
-        return GetFieldsPoints().ElementAt(highestIndex);
+        Debug.Log("시작과 제일 먼 필드는 (cost:" + highestCost + ") - " + fieldsPointsVertex.ElementAt(highestIndex).ToString());
+        return fieldsPointsVertex.ElementAt(highestIndex);
     }
 
 
