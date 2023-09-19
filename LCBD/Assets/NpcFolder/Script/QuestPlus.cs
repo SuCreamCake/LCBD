709a5c66809a4dd18687d83f04211ab5fefaa724
seunghoon <56308359+cho-seung-hoon@users.noreply.github.com>
2023-09-19 20:48:09 +0900

    NPC 변경
    천장있는 뽑기로 개선
    뽑으면 아이템 플레이어 위치에 아이템 생성

--- LCBD/Assets/NpcFolder/Script/QuestPlus.cs
+++ LCBD/Assets/NpcFolder/Script/QuestPlus.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class QuestPlus : MonoBehaviour
 {
-    // QuestData 클래스의 참조를 가져옴
-    public QuestData questData;
+    public int QuestId;
 
     private void Start()
     {
-
+        QuestId = 1;
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
-            // QuestData 클래스의 questDictionary에서 원하는 퀘스트의 ID를 사용하여 Quest 객체에 접근
-            if (questData.questDictionary.TryGetValue(1, out Quest quest))
-            {
-                // 해당 퀘스트의 currentCompletionCount를 증가시킴
-                quest.currentCompletionCount++;
-                Debug.Log("퀘스트 진행횟수 :" + quest.currentCompletionCount);
-
-                // 이제 해당 퀘스트의 진행도를 변경한 것이므로 원하는 처리를 수행할 수 있음
-                // 예: 퀘스트 클리어를 확인하고 다음 퀘스트로 이동 등
-            }
+            QuestManager.instance.Questing(QuestId);
         }
     }
 }
--- LCBD/Assets/NpcFolder/놀음꾼/뒤집기(뽑기)/FlipManager.cs
+++ LCBD/Assets/NpcFolder/놀음꾼/뒤집기(뽑기)/FlipManager.cs
@@ -7,20 +7,30 @@ public class FlipManager : MonoBehaviour
 {
     public CardFlip[] cardArray; // 카드 오브젝트 배열
     public Button ResetButton;
+    public Button UesButton;
+
+    public GameObject prefab;
 
     private void Start()
     {
         ResetButton.interactable = false;
-    }
 
-    public void AllFlip()
-    {
-        foreach (CardFlip card in cardArray)
+        for(int i = 0; i < cardArray.Length; i++)
         {
-            card.TrueFlip(); // 모든 카드에 대해 클릭 이벤트 실행
+            cardArray[i].canClickOff();
         }
 
-        StartCoroutine(EnableResetButtonAfterDelay(3f));
+    }
+
+    public void AllClickOff()
+    {
+        for (int i = 0; i < cardArray.Length; i++)
+        {
+            cardArray[i].canClickOff();
+        }
+
+
+        UesButton.interactable = true;
     }
 
     IEnumerator EnableResetButtonAfterDelay(float delay)
@@ -46,6 +56,48 @@ public class FlipManager : MonoBehaviour
     private void OnEnable()
     {
         Start();
+    }
+
+    public void UseButtonClick()
+    {
+        Debug.Log("뽑기 비용가져감");
+        for (int i = 0; i < cardArray.Length; i++)
+        {
+            cardArray[i].canClickOn();
+        }
+        UesButton.interactable = false;
+    }
+
+    public void Reward()
+    {
+        SpawnObjectAtPlayerPosition();
+    }
+
+    void SpawnObjectAtPlayerPosition()
+    {
+        // 플레이어 태그를 가진 오브젝트를 찾습니다.
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObject != null)
+        {
+            // 플레이어 오브젝트의 위치를 기준으로 프리팹을 인스턴스화합니다.
+            Vector3 playerPosition = playerObject.transform.position;
+            Vector3 NewPalyerPosition = playerPosition;
+            NewPalyerPosition.z -= 0.1f;
+
+            if (prefab != null)
+            {
+                Instantiate(prefab, NewPalyerPosition, Quaternion.identity);
+            }
+            else
+            {
+                Debug.LogError("프리팹 로드 실패: ");
+            }
+        }
+        else
+        {
+            Debug.LogError("플레이어 오브젝트를 찾을 수 없습니다. 'Player'가 없다.");
+        }
     }
 }
 
